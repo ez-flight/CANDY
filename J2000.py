689d7383f57734f318ed2e366270dd03bf9ec32c
Ez <ez.flight@yandex.ru>
2024-01-22 23:22:30 +0300

    22_2

--- J2000.py
+++ J2000.py
@@ -1,21 +1,24 @@
-import skyfield.sgp4lib as sgp4lib
-from astropy import coordinates as coord, units as u
+from astropy import coordinates as coord
+from astropy import units as u
 from astropy.time import Time
-from datetime import date, datetime
+from astropy import time
 
-time =  559889620.293
-# time- J2000 date
-# p,v- vectors, result of SGP4 in TEME frame
-date= datetime.datetime(2000, 1, 1, 12, 0) + datetime.timedelta(days=time - 2451545)
-p_0 = [-3219.2205314217895, 2841.7432608552854, -5795.244631928768]
-v_0 = [5.578288437467041, -2.398731696159303, -4.276547464171784]
-# Conversion from TEME to ITRS    
-p,v= sgp4lib.TEME_to_ITRF(time,np.asarray(p_0),np.asarray(v_0)*86400)
-v=v/86400
+def position_GCRS_J2000 (now, xyz):
+    cartrep = coord.CartesianRepresentation(*xyz, unit=u.km)
+    gcrs = coord.ITRS(cartrep, obstime=now)
+    itrs = gcrs.transform_to(coord.GCRS(obstime=now))
+    loc = coord.EarthLocation(*itrs.cartesian.xyz)
+    return loc
 
-# Conversion from ITRS to J2000    
-now = Time(date)
-itrs = coord.ITRS(p[0]*u.km, p[1]*u.km, p[2]*u.km, v[0]*u.km/u.s, v[1]*u.km/u.s, v[2]*u.km/u.s, obstime=now)
-gcrs = itrs.transform_to(coord.GCRS(obstime=now))
-#p,v=gcrs.cartesian.xyz.value,gcrs.velocity.d_xyz.value
-print (gcrs)
+
+def _test():
+
+    now = Time('2018-03-14 23:48:00')
+    # position of satellite in GCRS or J20000 ECI:
+    xyz=[-6340.40130292,3070.61774516,684.52263588]
+    loc = position_GCRS_J2000(now,xyz)
+    print(loc)
+
+
+if __name__ == "__main__":
+    _test()
