0e2fd4ad7bd4dbf0e8a38ca60b8af3501c1b0dbf
Ez <ez.flight@yandex.ru>
2024-01-24 18:17:51 +0300

    24_1

--- cal_cord_2.py
+++ cal_cord_2.py
@@ -3,6 +3,7 @@ import numpy
 from sgp4.earth_gravity import wgs84
 from datetime import date, datetime, timedelta
 
+from pyorbital.orbital import astronomy
 
 pi =3.14
 
@@ -26,22 +27,22 @@ def days(utc_time):
     minute = utc_time.minute
     second = utc_time.second
     if month < 3:
-        year = year -1
-        month = month + 12
-    b = -2 + int((year+4716)/4)-1179
-    d = (hour + (minute/60)+ (second/3600.0))/24.0
-    MJD = (365*year)- 679004 + b + int(30.6001*(month+1))+ day + d
+        year  -= 1
+        month += 12
+    b = int(year/400)-int(year/100)+int(year/4)
+    d = (hour + (minute/60.0) + (second/3600.0))/24.0
+    MJD = (365*year) - 679004 + b + int(30.6001*(month+1)) + day + d
     return MJD
 
-def gmst(utc_time):
+def gmsts(utc_time):
     PI2 = math.pi * 2    # => 6.283185307179586
     D2R = math.pi / 180  # => 0.017453292519943295
-    jd_ut1 = days(utc_time)
+    jd_ut1 = days(utc_time) # юлианская дата момента наблюдения
+ #   print (f"UTC TIME {utc_time}")
+ #   print (f"JD(UT1) - {jd_ut1}")
     t_ut1= (jd_ut1 - 2451545.0) / 36525
-    gmst =  67310.54841 \
-        + (876600.0 * 3600.0 + 8640184.812866 \
-        + (0.093104 \
-        -  6.2e-6 * t_ut1) * t_ut1) * t_ut1
+#    print (f"T промежуток времени в юлианских столетиях {t_ut1}")
+    gmst =  67310.54841+ (876600.0 * 3600.0 + 8640184.812866 + (0.093104 - 6.2e-6 * t_ut1) * t_ut1) * t_ut1
     gmst = (gmst * D2R / 240.0) % PI2
     if gmst < 0.0:
         gmst += PI2
@@ -54,15 +55,15 @@ def GMST(utc_time): #гринвичское звёздное время
     GMST = GMST-360*int(GMST/360)
     if GMST<0:
         GMST = 360.0+GMST
-    return GMST
+    return GMST*pi/180.0
 
 
 def ISKtoGSK(utc_time, x,y,z): # Перевод из инерциальной системы в Гринвеческую
-    alfa = GMST(utc_time)*pi/180.0
-    alfa_2 = gmst(utc_time)
-    print (f"Угол ={alfa} {alfa_2} в время {utc_time}")
-    cz = math.cos(alfa_2) #alfa - гринвичское звёздное время в радианах
-    sz = math.sin(alfa_2)
+    alfa = astronomy.gmst(utc_time)
+ #   alfa_2 = gmst(utc_time)
+ #   print (f"Угол ={alfa} {alfa_2} в время {utc_time}")
+    cz = math.cos(alfa) #alfa - гринвичское звёздное время в радианах
+    sz = math.sin(alfa)
 
     Rz = ([
     [cz,sz,0],
@@ -75,12 +76,10 @@ def ISKtoGSK(utc_time, x,y,z): # Перевод из инерциальной с
 
 
 def GSKtoISK(utc_time, x,y,z): # Перевод из Гринвеческой СК в инерциальную СК
-    alfa = GMST(utc_time)*pi/180.0 #alfa - гринвичское звёздное время в радианах
-#    print (f"Угол ={alfa} в время {utc_time}")
-
-    alfa_2 = gmst(utc_time)
-    cz = math.cos(alfa_2) 
-    sz = math.sin(alfa_2)
+    alfa = astronomy.gmst(utc_time)
+    print (f"Угол ={alfa}  в время {utc_time}")
+    cz = math.cos(alfa) #alfa - гринвичское звёздное время в радианах
+    sz = math.sin(alfa)
 
     Rz = ([
     [cz,sz,0],
@@ -88,16 +87,14 @@ def GSKtoISK(utc_time, x,y,z): # Перевод из Гринвеческой С
     [0,0,1]])
 
     position = x,y,z
- #   A = np.matrix('1 -3; 2 5')
     Rz_inv = numpy.linalg.inv(Rz)
- #   Rz_0 = numpy.array(Rz)
     p =  numpy.dot(position,Rz_inv)
     return p[0],p[1],p[2]
 
 def _test():
 
-    delta = timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=1, hours=0, weeks=0)
-    dt_start = datetime.now()
+    delta = timedelta(days=0, seconds=0.5, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
+    dt_start = datetime.utcnow()
     dt_end = dt_start + timedelta(
         days=1,
         seconds=0,
@@ -111,7 +108,15 @@ def _test():
 
     xyz=[-6340.40130292,3070.61774516,684.52263588]
     while dt<dt_end:
-        p = ISKtoGSK(dt,xyz[0], xyz[1], xyz[2])
+       # p = GSKtoISK(dt,xyz[0], xyz[1], xyz[2])
+#    print(GMST(dt_start))
+ #   print(f"GMST = {gmst(dt_start)}")
+        if astronomy.gmst(dt) < 0.0001:
+            print(f"1 -> {astronomy.gmst(dt)} на {dt}")
+        if gmsts(dt) < 0.0001:
+            print(f"2 -> {gmsts(dt)} на {dt}")
+        if GMST(dt) < 0.0001:
+            print(f"3 -> {GMST(dt)} на {dt}")
 #        print (p)
 #        print (p_0)
         dt += delta
