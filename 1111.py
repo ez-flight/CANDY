03faba6dd153365355a5949024696849a1d18a8a
Константин Кудинов <ez.flight@yandex.ru>
2024-02-08 06:42:52 +0300

    07_02_03

--- 1111.py
+++ 1111.py
@@ -7,7 +7,7 @@ from calc_F_L import calc_f_doplera, calc_lamda
 from candy import get_lat_lon_sgp, get_position, get_xyzv_from_latlon
 
 
-def create_orbital_track_for_f_doplera(tle_1, tle_2, dt_start, dt_end, delta, pos_t, Lam_f):
+def create_orbital_track_for_f_doplera(tle_1, tle_2, dt_start, pos_t, Lam_f):
      
     lon_t, lat_t, alt_t  = pos_t
 #    Fd=0.0
@@ -23,38 +23,28 @@ def create_orbital_track_for_f_doplera(tle_1, tle_2, dt_start, dt_end, delta, po
     # Объявляем счётчики, i для идентификаторов, minutes для времени
     i = 0
     mas = []
+    # Считаем положение спутника в инерциальной СК
+    X_s, Y_s, Z_s, Vx_s, Vy_s, Vz_s = get_position(tle_1, tle_2, dt)
+    Rs = X_s, Y_s, Z_s
+    Vs = Vx_s, Vy_s, Vz_s
+    # Считаем положение спутника в геодезической СК
+    lon_s, lat_s, alt_s = get_lat_lon_sgp(tle_1, tle_2, dt)
+    #Персчитываем положение объекта из геодезической в инерциальную СК  на текущее время с расчетом компонентов скорости точки на земле
+    pos_t, v_t = get_xyzv_from_latlon(dt, lon_t, lat_t, alt_t)
+    X_t, Y_t, Z_t = pos_t
+    #Расчет ----
+    R_s = math.sqrt((X_s**2)+(Y_s**2)+(Z_s**2))
+    R_0 = math.sqrt(((X_s-X_t)**2)+((Y_s-Y_t)**2)+((Z_s-Z_t)**2))
+    R_e = math.sqrt((X_t**2)+(Y_t**2)+(Z_t**2))
+    V_s = math.sqrt((Vx_s**2)+(Vy_s**2)+(Vz_s**2))
+    #Расчет двух углов
+    y = math.acos(((R_0**2)+(R_s**2)-(R_e**2))/(2*R_0*R_s))
+    y_grad = y * (180/math.pi)
+    ay = math.acos(((R_0*math.sin(y))/R_e))
+    ay_grad = math.degrees(ay)
+    Fd = calc_f_doplera (Lam_f, Lam, ay, Rs, Vs, R_0, R_s, R_e, V_s)
 
-    while dt < dt_end:
-        # Считаем положение спутника в инерциальной СК
-        X_s, Y_s, Z_s, Vx_s, Vy_s, Vz_s = get_position(tle_1, tle_2, dt)
-        Rs = X_s, Y_s, Z_s
-        Vs = Vx_s, Vy_s, Vz_s
-
-        # Считаем положение спутника в геодезической СК
-        lon_s, lat_s, alt_s = get_lat_lon_sgp(tle_1, tle_2, dt)
-
-        #Персчитываем положение объекта из геодезической в инерциальную СК  на текущее время с расчетом компонентов скорости точки на земле
-        pos_t, v_t = get_xyzv_from_latlon(dt, lon_t, lat_t, alt_t)
-        X_t, Y_t, Z_t = pos_t
-
-        #Расчет ----
-        R_s = math.sqrt((X_s**2)+(Y_s**2)+(Z_s**2))
-        R_0 = math.sqrt(((X_s-X_t)**2)+((Y_s-Y_t)**2)+((Z_s-Z_t)**2))
-        R_e = math.sqrt((X_t**2)+(Y_t**2)+(Z_t**2))
-        V_s = math.sqrt((Vx_s**2)+(Vy_s**2)+(Vz_s**2))
-
-        #Расчет двух углов
-        y = math.acos(((R_0**2)+(R_s**2)-(R_e**2))/(2*R_0*R_s))
-        y_grad = y * (180/math.pi)
-        ay = math.acos(((R_0*math.sin(y))/R_e))
-        ay_grad = math.degrees(ay)
- 
-        Fd = calc_f_doplera (Lam_f, Lam, ay, Rs, Vs, R_0, R_s, R_e, V_s)
-        mas.append ([ i, dt, lon_s, lat_s, R_s, R_e, R_0, y_grad, ay_grad, Lam_f, Fd])
-        #print (f"{Fd:.5f}")
-        i += 1
-        dt += delta
-    return mas   
+    return  dt, lon_s, lat_s, R_s, R_e, R_0, y_grad, ay_grad, Lam_f, Fd
 
 
 
@@ -86,14 +76,6 @@ def _test():
         hours=0,
         weeks=0
     )
-    mas = []
-    #Координаты объекта в геодезической СК (lat,lon, alt)
-    pos_t_1 = 59.95, 30.316667, 12
-    pos_t_2 = 61.796111, 34.349167, 112
-    mas1 = create_orbital_track_for_f_doplera(tle_1, tle_2, dt_start, dt_end, delta, pos_t_1, Lam_f=90)
-    mas2 = create_orbital_track_for_f_doplera(tle_1, tle_2, dt_start, dt_end, delta, pos_t_2, Lam_f=90)
-    mas = mas1 + mas2
-    mas3 = sorted(mas, reverse=True, key=lambda x: x[10])
     
     filename = "space/proba.shp"
     track_shape = shapefile.Writer(filename, shapefile.POINT)
@@ -112,9 +94,21 @@ def _test():
     track_shape.field("φ", "F", 40, 5)
     track_shape.field("λ", "F", 40, 5)
     track_shape.field("f", "F", 40, 5)
+
+
+    #Координаты объекта в геодезической СК (lat,lon, alt)
+    pos_t_1 = 59.95, 30.316667, 12
+    pos_t_2 = 61.796111, 34.349167, 112
     
-    track_shape.point(lon_s, lat_s)
-    track_shape.record(i, dt, lon_s, lat_s, R_s, R_t, R_n, f_grad)
+    i = 0
+
+    while dt_start < dt_end:
+        date, lon_s, lat_s, R_s, R_e, R_0, y_grad, ay_grad, Lam_f, Fd = create_orbital_track_for_f_doplera(tle_1, tle_2, dt_start, dt_end, delta, pos_t_1, Lam_f=90)
+        track_shape.point(lon_s, lat_s)
+        track_shape.record(i, date, lon_s, lat_s, R_s, R_e, R_0, y_grad, ay_grad, Lam_f, Fd)
+        i += 1
+        dt_start += delta
+
 
     try:
         # Создаем файл .prj с тем же именем, что и выходной .shp
